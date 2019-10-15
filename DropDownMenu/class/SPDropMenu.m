aade50b2c9856cb748e16969538bd5fc171007d0
lincf0912 <dayflyking@163.com>
2019-10-15 15:10:38 +0800

    -调整属性

--- DropDownMenu/class/SPDropMenu.m
+++ DropDownMenu/class/SPDropMenu.m
@@ -12,11 +12,14 @@
 static SPDropMainMenu *_SPDropMainMenu;
 
 const NSString *SPDropMainMenu_autoDismiss = @"SPDropMainMenu_autoDismiss";
+const NSString *SPDropMainMenu_backgroundColor = @"SPDropMainMenu_backgroundColor";
+const NSString *SPDropMainMenu_direction = @"SPDropMainMenu_direction";
 
 static NSMutableDictionary *_SPDrapMainMenuPropertys;
 
 @implementation SPDropMenu
 
+#pragma mark - preporty
 + (void)setAutoDismiss:(BOOL)isAutoDismiss
 {
     self.SPDrapMainMenuPropertys[SPDropMainMenu_autoDismiss] = @(isAutoDismiss);
@@ -28,23 +31,39 @@ static NSMutableDictionary *_SPDrapMainMenuPropertys;
     return (BOOL)_SPDropMainMenu;
 }
 
++ (void)setBackgroundColor:(UIColor *)color
+{
+    self.SPDrapMainMenuPropertys[SPDropMainMenu_backgroundColor] = color;
+    _SPDropMainMenu.containerViewbackgroundColor = color;
+}
++ (void)setDirection:(SPDropMainMenuDirection)direction
+{
+    self.SPDrapMainMenuPropertys[SPDropMainMenu_direction] = @(direction);
+}
+
+#pragma mark - function
 + (void)showInView:(UIView *)view items:(NSArray <id <SPDropItemProtocol>>*)items
 {
-    [self dismiss];
+    [self dismissWithAnimated:NO];
     _SPDropMainMenu = [self SPDropMainMenuWithItems:items];
     [_SPDropMainMenu showInView:view];
 }
 + (void)showFromPoint:(CGPoint)point items:(NSArray <id <SPDropItemProtocol>>*)items
 {
-    [self dismiss];
+    [self dismissWithAnimated:NO];
     _SPDropMainMenu = [self SPDropMainMenuWithItems:items];
     [_SPDropMainMenu showFromPoint:point];
 }
 
 + (void)dismiss
 {
+    [self dismissWithAnimated:YES];
+}
+
++ (void)dismissWithAnimated:(BOOL)animated
+{
     if (_SPDropMainMenu) {
-        [_SPDropMainMenu dismiss];
+        [_SPDropMainMenu dismissWithAnimated:animated];
         _SPDropMainMenu = nil;
     }
 }
@@ -64,6 +83,14 @@ static NSMutableDictionary *_SPDrapMainMenuPropertys;
     id value = self.SPDrapMainMenuPropertys[SPDropMainMenu_autoDismiss];
     if (value) {
         dropMainMenu.autoDismiss = [value boolValue];
+    }
+    value = self.SPDrapMainMenuPropertys[SPDropMainMenu_backgroundColor];
+    if (value) {
+        dropMainMenu.containerViewbackgroundColor = value;
+    }
+    value = self.SPDrapMainMenuPropertys[SPDropMainMenu_direction];
+    if (value) {
+        dropMainMenu.menuDirection = [value integerValue];
     }
     for (id <SPDropItemProtocol> item in items) {
         [dropMainMenu addItem:item];
